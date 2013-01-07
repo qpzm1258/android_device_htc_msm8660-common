d8c02fcb9e4b5f66fadb0c8c307e27f0d85cfc8b
Dorian Snyder <dastin1015@gmail.com>
2013-01-06 19:17:54 -0800

    msm8660-common: remove duplicate board vendor define

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -12,8 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-BOARD_VENDOR := htc
-
 TARGET_SPECIFIC_HEADER_PATH := device/htc/msm8660-common/include
 
 # Bootloader
