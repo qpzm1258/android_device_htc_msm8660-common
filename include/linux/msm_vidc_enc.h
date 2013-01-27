c78991cff7f9facb3a7d91f89512085765bd4277
Dorian Snyder <dastin1015@gmail.com>
2013-01-26 20:13:59 -0800

    msm8660-common: add VEN_IOCTL_SET_SPS_PPS_FOR_IDR

--- include/linux/msm_vidc_enc.h
+++ include/linux/msm_vidc_enc.h
@@ -259,6 +259,7 @@ struct venc_ioctl_msg{
 #define VEN_IOCTL_SET_EXTRADATA _IOW(VEN_IOCTLBASE_ENC, 48, struct venc_ioctl_msg)
 #define VEN_IOCTL_GET_EXTRADATA _IOR(VEN_IOCTLBASE_ENC, 49, struct venc_ioctl_msg)
 #define VEN_IOCTL_SET_SLICE_DELIVERY_MODE _IO(VEN_IOCTLBASE_ENC, 50)
+#define VEN_IOCTL_SET_SPS_PPS_FOR_IDR _IOW(VEN_IOCTLBASE_ENC, 51, struct venc_ioctl_msg)
 struct venc_switch{
 /* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
  unsigned char status;
