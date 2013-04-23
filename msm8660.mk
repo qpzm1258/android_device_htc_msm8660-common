393529d123b0cf4918bd4b215de8df379ce0b26c
Dărescu Ionuț <johnny@esi.ro>
2013-04-23 14:07:23 +0300

    Update msm8660.mk

--- msm8660.mk
+++ msm8660.mk
@@ -127,7 +127,10 @@ PRODUCT_AAPT_PREF_CONFIG := hdpi
 # Common build properties
 PRODUCT_PROPERTY_OVERRIDES += \
     com.qc.hardware=true \
+    debug.composition.type=dyn \
+    debug.enabletr=true \
     debug.egl.hw=1 \
+    debug.mdpcomp.maxlayer=0 \
     debug.mdpcomp.logs=0 \
     debug.sf.hw=1 \
     dev.pm.dyn_samplingrate=1 \
