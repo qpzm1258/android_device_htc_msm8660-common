3d9a3574ceaf9db61ab399bb719d1c3e7af0593c
Dărescu Ionuț <ionut.darescu@gmail.com>
2014-03-21 16:31:25 +0200

    Cleanup flags

--- msm8660.mk
+++ msm8660.mk
@@ -129,17 +129,7 @@ PRODUCT_AAPT_PREF_CONFIG := hdpi
 # Common build properties
 PRODUCT_PROPERTY_OVERRIDES += \
     com.qc.hardware=true \
-    debug.enabletr=true \
-    debug.egl.hw=1 \
-    debug.mdpcomp.maxlayer=3 \
-    debug.hwc.dynThreshold=1.9 \
-    persist.hwc.mdcomp.enable=true \
-    debug.mdpcomp.logs=0 \
-    debug.sf.hw=1 \
-    dev.pm.dyn_samplingrate=1 \
     ro.opengles.version=131072 \
-    ro.zygote.disable_gl_preload=true \
-    debug.hwui.render_dirty_regions=false \
     debug.egl.recordable.rgba8888=1 \
     dalvik.vm.dexopt-data-only=0
 
