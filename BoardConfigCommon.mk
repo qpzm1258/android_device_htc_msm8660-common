4760692e1c5f2ce5fcc656d5a54397aecfd9f08a
Darescu Ionut <ionut.darescu@gmail.com>
2014-05-12 12:32:15 -0400

    Build kernel with sabermod 4.8
    Change-Id: Id5171052436ef57c5e98da43adfd003a04e5816f

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -23,7 +23,7 @@ TARGET_NO_BOOTLOADER := true
 
 # Kernel
 TARGET_KERNEL_SOURCE := kernel/htc/shooteru
-TARGET_KERNEL_CUSTOM_TOOLCHAIN := linaro-4.7.3_cortex-a8
+TARGET_KERNEL_CUSTOM_TOOLCHAIN := sm-arm-eabi-4.8
 
 # Platform
 TARGET_BOARD_PLATFORM := msm8660
