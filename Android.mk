c862fc976126934359b441d51f6db2f63144eb3f
Li shuhua <qpzm1258@163.com>
2014-06-12 10:22:35 +0800

    support shooterct

--- Android.mk
+++ Android.mk
@@ -14,7 +14,7 @@
 # limitations under the License.
 #
 
-ifneq ($(filter shooter shooteru shootervm pyramid holiday ruby doubleshot,$(TARGET_DEVICE)),)
+ifneq ($(filter shooter shooterct shootervm pyramid holiday ruby doubleshot,$(TARGET_DEVICE)),)
 
 LOCAL_PATH := $(call my-dir)
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -22,7 +22,7 @@ TARGET_SPECIFIC_HEADER_PATH := device/htc/msm8660-common/include
 TARGET_NO_BOOTLOADER := true
 
 # Kernel
-TARGET_KERNEL_SOURCE := kernel/htc/shooteru
+TARGET_KERNEL_SOURCE := kernel/htc/shooterct
 TARGET_KERNEL_CUSTOM_TOOLCHAIN := linaro-arm-eabi-4.9
 
 # Platform
